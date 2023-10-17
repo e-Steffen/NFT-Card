a7c9d872ad67c9bc89c04e97df9cf2c4b95062e1
Steffen Zierold <steffen.zierold@outlook.com>
2023-10-17 22:12:15 +0200

    cursor-Pointer added via js to imgF

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const imgF = document.querySelector(".heroframe");
 const img = document.querySelector(".hero");
 const imgEye = document.querySelector(".eye");
+const cursorPointer = document.querySelector(".heroframe");
 
 imgF.addEventListener("mouseenter", () => {
   imgEye.style.display = "block";
   img.style.opacity = "50%";
+  cursorPointer.style.cursor = "pointer";
 });
 
 imgF.addEventListener("mouseleave", () => {
